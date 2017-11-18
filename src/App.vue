b007fcf35dff7af526e761128689665593d722d6
Kudouhei <819457830@qq.com>
2017-11-18 19:26:47 +0800

    init
    init version

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <image-slider :images="images" :params="{minMoveDistance: 50, autoPlay: true}"></image-slider>
+  </div>
+</template>
+
+<script>
+import ImageSlider from './components/ImageSlider.vue'
+import first from './assets/first.jpeg'
+import sec from './assets/sec.jpeg'
+import three from './assets/three.jpeg'
+import four from './assets/four.jpeg'
+import five from './assets/five.jpeg'
+
+export default {
+  name: 'app',
+  components: {
+    ImageSlider
+  },
+  data () {
+    return {
+      images: 
+      [
+        first,
+        sec,
+        three,
+        four,
+        five
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+</style>
